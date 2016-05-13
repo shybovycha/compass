cf62ae7fc1325aef1cb1a6e90c97c9680596aebc
Artem Shoobovych <shybovycha@gmail.com>
2016-05-13 11:23:58 +0200

    Added watchify. Added matrialize CSS to enforce some funky stuff like grids, media queries, etc.

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -2,24 +2,27 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
 var browserify = require('browserify');
 var gutil = require('gulp-util');
-//var watchify = require('watchify');
+var watchify = require('watchify');
 //var reactify = require('reactify');
 //var concat = require('gulp-concat');
 
-gulp.task('browserify', function() {
-    var bundler = browserify({
-	entries: './src/App.jsx',
-	debug: true,
-	extensions: ['.jsx'] 
-    });
-
-    bundler
-	.transform("babelify", { presets: ["es2015", "react"] })
-	.bundle()
-	.on('error', gutil.log)
-	.pipe(source('bundle.js'))
-	.pipe(gulp.dest('./build'));
+var browserifyOptions = Object.assign(watchify.args, {
+    entries: './src/App.jsx',
+    debug: true,
+    extensions: ['.jsx'] 
 });
 
-// Just running the two tasks
-gulp.task('default', ['browserify']);
+var watcher = watchify(browserify(browserifyOptions));
+
+function bundle() {
+    return watcher
+        .transform("babelify", { presets: ["es2015", "react"] })
+        .bundle()
+        .on('error', gutil.log)
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./build'));
+};
+
+watcher.on('update', bundle)
+
+gulp.task('default', bundle);
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeComponent from './MyAwesomeComponent';
+
+import Dashboard from './Dashboard';
 
 const App = () => (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <MyAwesomeComponent />
+        <Dashboard title="Dashboard">
+            <h1>Hello, World</h1>
+        </Dashboard>
     </MuiThemeProvider>
 );
 
